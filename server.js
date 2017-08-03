c9f3ac5ce7478a4e338258b34e148726c8345460
Eri Nagase <stareri@hotmail.com>
2017-08-03 09:32:25 -0700

    editing index.js and config.json

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var routes = require("./controller/burger_controller.js");
 
 app.use("/", routes);
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
